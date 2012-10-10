54db0f90ea2749b850ee66fc150a53938b48ce50
lab-08 <lab-01@ama-desktop.(none)>
2012-10-10 10:06:12 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Academiarails::Application.config.secret_token = '50bda590c6b045d6be4c89caf5e0f4a97687e6eb24fd7b536a6a93e8e976c2883bc62e36fe890682c3c958f61b09291a7a42735eab557d0e1c69d91d515dbb22'
